a58d86e7a1c7ba84cda87993afab17e0bb541900
jdyrussellinvestments <31643511+jdyrussellinvestments@users.noreply.github.com>
2018-01-10 13:35:11 +1100

    updated modal for mobile

---
+++ js/button.js
@@ -0,0 +1,20 @@
+
+
+      function downloadFunction() {
+          //alert("form 1");
+          
+          //Reset iFrame source
+          $('#idownload').attr('src', '');
+          //Set iFrame source
+          $('#idownload').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en');
+      }
+      
+      function callbackFunction() {
+          //alert("form 2");
+          
+          //Reset iFrame source
+          $('#iCallback').attr('src', '');
+          //Set iFrame source
+          $('#iCallback').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en');
+      }
+
