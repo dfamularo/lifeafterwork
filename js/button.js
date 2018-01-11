2dea5486cd96aecd01e4a4497a644efd223a9ffe
jdyrussellinvestments <31643511+jdyrussellinvestments@users.noreply.github.com>
2018-01-11 15:30:06 +1100

    add ios detection to open form in a new window

--- js/button.js
+++ js/button.js
@@ -1,20 +1,60 @@
 
+var standalone = window.navigator.standalone,
+    userAgent = window.navigator.userAgent.toLowerCase(),
+    safari = /safari/.test( userAgent ),
+    ios = /iphone|ipod|ipad/.test( userAgent );
 
-      function downloadFunction() {
-          //alert("form 1");
-          
-          //Reset iFrame source
-          $('#idownload').attr('src', '');
-          //Set iFrame source
-          $('#idownload').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en');
-      }
+if( ios ) {
+    
+    if ( !standalone && safari ) {
+        
+        //document.getElementById( 'where-am-i' ).textContent = 'browser';
+        
+    } else if ( standalone && !safari ) {
+        
+        //document.getElementById( 'where-am-i' ).textContent = 'standalone';
+
+        
+        //Reset iFrame source
+        $('#idownload').attr('src', '');
+
+        //OPEN IFRAME IN A NEW WINDOW
+
+        //Set iFrame source
+        $('#idownload').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en').attr("target","_blank");
+
+
+        
+    } else if ( !standalone && !safari ) {
+        
+        //document.getElementById( 'where-am-i' ).textContent = 'uiwebview';
+        function downloadFunction() {
+            //alert("form 1");
+            
+            //Reset iFrame source
+            $('#idownload').attr('src', '');
+            //Set iFrame source
+            $('#idownload').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en');
+        }
+        
+        function callbackFunction() {
+            //alert("form 2");
+            
+            //Reset iFrame source
+            $('#iCallback').attr('src', '');
+            //Set iFrame source
+            $('#iCallback').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en');
+        }
+        
+    };
+    
+} else {
+    
+    //document.getElementById( 'where-am-i' ).textContent = 'not iOS';
+    
+};
+
+
+
       
-      function callbackFunction() {
-          //alert("form 2");
-          
-          //Reset iFrame source
-          $('#iCallback').attr('src', '');
-          //Set iFrame source
-          $('#iCallback').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en');
-      }
 
