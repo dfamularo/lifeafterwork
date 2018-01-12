eaec088e666102d2bb4100408caf862879335b17
jdyrussellinvestments <31643511+jdyrussellinvestments@users.noreply.github.com>
2018-01-12 17:36:43 +1100

    added google tag

--- js/button.js
+++ js/button.js
@@ -1,23 +1,21 @@
 
-var urldownload = "https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en";
-var urlcallback = "https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en"
+var urldownload = "https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en",
+    urlcallback = "https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en",
+    // Get the user agent string
+    deviceAgent = navigator.userAgent,
+    // Set var to iOS device name or null
+    ios = deviceAgent.toLowerCase().match(/(iphone|ipod|ipad)/);
 
-// Get the user agent string
-var deviceAgent = navigator.userAgent;
-// Set var to iOS device name or null
-var ios = deviceAgent.toLowerCase().match(/(iphone|ipod|ipad)/);
 
-
+// EVENT HANDLERS
 function downloadFunction() {
     //alert("form 1");
-
     if (ios) {
         // This is the line that matters
         //alert("iPhone detected");
         $('#idownload').attr("disabled", "disabled");
 
         var downloadNewWindow = window.open(urldownload,"Download","location=yes,menubar=yes");
-
 
         //$(this).attr('href', urldownload);
     } else {
@@ -26,20 +24,17 @@ function downloadFunction() {
         $('#idownload').attr('src', '');
         //Set iFrame source
         $('#idownload').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=58655&lang=en');
-    }
-        
+    }    
 }
 
 function callbackFunction() {
     //alert("form 2");
-
     if (ios) {
         // This is the line that matters
         //alert("iPhone detected");
         $('#iCallback').attr("disabled", "disabled");
 
         var callbackNewWindow = window.open(urlcallback,"Callback","location=yes,menubar=yes");
-
 
         //$(this).attr('href', urlcallback);
     } else {
@@ -48,6 +43,7 @@ function callbackFunction() {
         $('#iCallback').attr('src', '');
         //Set iFrame source
         $('#iCallback').attr('src', 'https://russellinvestments.net.au/survey/index.php?sid=16915&lang=en');
-    }
-    
+    } 
 }
+
+
